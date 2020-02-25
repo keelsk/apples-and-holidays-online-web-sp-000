9a13be6aa902c1d0c83b63164b5229f31b8a8352
Kirsten Keels <keelsk@gmail.com>
2020-02-25 23:40:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,11 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, supply_hash|
     puts "#{season.capitalize()}:"
     supply_hash.each do |holiday, supply|
-      new_array = holiday.split("_")
-      capital_array = new_array.each do |word|
-        word.capitalize()
-      end
-      puts "  #{capital_array.join(' ')}: #{supply.join(", ")}"
+      puts "  #{holiday.capitalize()}: #{supply.join(", ")}"
     end
   end
 
