3d58b71cca6ff7d8353c1f21cc63dab47d4a4370
Kirsten Keels <keelsk@gmail.com>
2020-02-26 00:05:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, supply_hash|
     supply_hash.select do |holiday, supply| 
       if supply.include?("BBQ")
-        holiday_hash[season][holiday]
+        puts "it has barbeque"
       end
     end
   end
